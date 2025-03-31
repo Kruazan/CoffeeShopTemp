f260a435eaa6c6f8d92375cea94f072e80aa8d03
Kruazan <zandermr6@gmail.com>
2025-03-31 03:23:46 +0300

    last commit (work version ) SWAGGER + LOGS + WARN_HANDLERS

---
+++ src/main/java/com/example/coffeeshop/exception/UserUpdateException.java
@@ -0,0 +1,9 @@
+package com.example.coffeeshop.exception;
+
+/**DA.*/
+public class UserUpdateException extends RuntimeException {
+    /**Yes.*/
+    public UserUpdateException(String message) {
+        super(message);
+    }
+}
